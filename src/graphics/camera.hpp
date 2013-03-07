7633733df608f5de143479bea6841f7846f51e00
Leo Muona <leo.muona@gmail.com>
2013-03-07 20:26:28 +0200

    camera model

---
+++ src/graphics/camera.hpp
@@ -0,0 +1,24 @@
+#include "math/vec3.hpp"
+
+namespace skeletor {
+namespace graphics {
+
+class Camera {
+private:
+        math::Vec3f m_position;
+        math::Vec3f m_up;
+        math::Vec3f m_lookAt;
+
+public:
+        Camera();
+
+        void setPosition(const math::Vec3f &m_position);
+        void setLookAt(const math::Vec3f &lookAt);
+        void setUp(const math::Vec3f &up);
+        math::Vec3f getPosition();
+        math::Vec3f getLookAt();
+        math::Vec3f getUp();
+};
+
+}; // namespace graphics
+}; // namespace skeletor
