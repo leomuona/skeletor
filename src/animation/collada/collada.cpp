7913f0030aab97c125ebfd4192da167b5458ff4c
Jesse Niemistö <jesse.niemi27@gmail.com>
2013-04-22 21:23:42 +0300

    Collada parser identifies the authoring tool.

--- src/animation/collada/collada.cpp
+++ src/animation/collada/collada.cpp
@@ -3,6 +3,7 @@
 #include "animation/collada/collada_library_visual_scene.hpp"
 #include "animation/joint.hpp"
 #include "animation/skeleton.hpp"
+#include "util/string.hpp"
 
 #include "rapidxml.hpp"
 #include "rapidxml_utils.hpp"
@@ -10,6 +11,22 @@
 namespace skeletor {
 namespace animation {
 namespace collada {
+
+std::string getAuthoringTool(rapidxml::xml_node<> *root)
+{
+	using namespace rapidxml;
+	xml_node<> *asset = root->first_node("asset");
+	while (asset != NULL) {
+		xml_node<> *cunt = asset->first_node("contributor");
+		if (cunt) {
+			return cunt->first_node("authoring_tool")->value();
+		}
+
+		asset = asset->next_sibling("channel");
+	}
+
+	return "";
+}
 
 skeletor::animation::Skeleton *loadSkeleton(const std::string &filename)
 {
@@ -21,6 +38,8 @@ skeletor::animation::Skeleton *loadSkeleton(const std::string &filename)
 	doc.parse<0>(xmlFile.data());
 
 	xml_node<> *root = doc.first_node("COLLADA");
+	std::string authoring_tool = getAuthoringTool(root);
+	std::cout << filename << ": authoring tool used: " << authoring_tool << std::endl;
 
 	Joint *root_joint = load_library_visual_scenes(root);
 	skeleton = new Skeleton(root_joint);
@@ -28,7 +47,12 @@ skeletor::animation::Skeleton *loadSkeleton(const std::string &filename)
 	AnimationLibrary *lib = load_library_animations(root);
 	animationLibraryToKeyFrameAnimation(*lib, *skeleton);
 
-	skeleton->setupBindPoseMatrices();
+	util::toLower(authoring_tool);
+	if (util::beginsWith(authoring_tool, "maya")) {
+		skeleton->setupBindPoseMatrices();
+	} else if (util::beginsWith(authoring_tool, "blender")) {
+		// blender specific stuff.
+	}
 
 	return skeleton;
 }
