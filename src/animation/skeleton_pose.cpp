bb7fe52acb96518196455786ab4567e973843772
Jesse Niemistö <jesse.niemi27@gmail.com>
2013-04-23 20:40:33 +0300

    Forgot to add this in 26a6a811.

--- src/animation/skeleton_pose.cpp
+++ src/animation/skeleton_pose.cpp
@@ -30,12 +30,6 @@ void SkeletonPose::apply(float time)
 	for (int i=0; i<joints.size(); ++i) {
 		const Joint *joint = joints[i].first;
 		float t = fmod(time, joint->getMaxTime());
-
-		if (isnan(t)) {
-			m_localPoses.insert(std::pair<const Joint *, math::Mat4x4f>(joint, math::Mat4x4f()));
-			continue;
-		}
-
 		std::pair<KeyFrame, KeyFrame> keyframes(joint->find(t));
 		KeyFrame k = keyframes.first.lerp(keyframes.second, t);
 		m_localPoses.insert(std::pair<const Joint *, math::Mat4x4f>(joint, k.getTransform()));
